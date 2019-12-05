06cd156db659a9e6f04df16a049547003ab425dd
Administrator <Administrator@MSDN-SPECIAL>
2019-12-05 15:18:41 +0900

    [EN_genius] 팀프로젝트

---
+++ EN_genius/src/test/java/teamproject/en/genius/EnGeniusApplicationTests.java
@@ -0,0 +1,13 @@
+package teamproject.en.genius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnGeniusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
