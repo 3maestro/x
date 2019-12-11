b8f5461f7f7f747e79c7912ad80c52a3c3564ed7
Administrator <Administrator@MSDN-SPECIAL>
2019-12-11 13:56:12 +0900

    [정상혁] 1차 병합 2019-12-11 13:56

---
+++ EN_genius/src/main/java/com/cafe24/radev/vo/Part.java
@@ -0,0 +1,61 @@
+package com.cafe24.radev.vo;
+
+public class Part {
+	private String partNumber;
+	private String bigCate;
+	private String midCate;
+	private String partName;
+	private String partMany;
+	private String partPrice;
+	private String partWrite;
+	private String partUpdateDate;
+	
+	public String getPartNumber() {
+		return partNumber;
+	}
+	public void setPartNumber(String partNumber) {
+		this.partNumber = partNumber;
+	}
+	public String getBigCate() {
+		return bigCate;
+	}
+	public void setBigCate(String bigCate) {
+		this.bigCate = bigCate;
+	}
+	public String getMidCate() {
+		return midCate;
+	}
+	public void setMidCate(String midCate) {
+		this.midCate = midCate;
+	}
+	public String getPartName() {
+		return partName;
+	}
+	public void setPartName(String partName) {
+		this.partName = partName;
+	}
+	public String getPartMany() {
+		return partMany;
+	}
+	public void setPartMany(String partMany) {
+		this.partMany = partMany;
+	}
+	public String getPartPrice() {
+		return partPrice;
+	}
+	public void setPartPrice(String partPrice) {
+		this.partPrice = partPrice;
+	}
+	public String getPartWrite() {
+		return partWrite;
+	}
+	public void setPartWrite(String partWrite) {
+		this.partWrite = partWrite;
+	}
+	public String getPartUpdateDate() {
+		return partUpdateDate;
+	}
+	public void setPartUpdateDate(String partUpdateDate) {
+		this.partUpdateDate = partUpdateDate;
+	}
+}
